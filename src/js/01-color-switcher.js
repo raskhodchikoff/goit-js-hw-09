6397015d626861d79ac0aca245a95038ac9afad2
raskhodchikoff <artlan@ukr.net>
2022-08-11 14:48:32 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+class ColorSwitcher {
+  constructor(changeColor) {
+    this.intervalID = null;
+    this.isActive = false;
+    this.changeColor = changeColor;
+    refs.stopBtn.disabled = true;
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+
+    this.isActive = true;
+    this.intervalID = setInterval(() => changeColor(getRandomHexColor()), 1000);
+  }
+
+  stop() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+
+    clearInterval(this.intervalID);
+    this.isActive = false;
+  }
+}
+
+const colorSwitcher = new ColorSwitcher();
+
+refs.startBtn.addEventListener(
+  'click',
+  colorSwitcher.start.bind(colorSwitcher)
+);
+refs.stopBtn.addEventListener('click', colorSwitcher.stop.bind(colorSwitcher));
